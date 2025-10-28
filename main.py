cdc54c904a62b80c7acbbe61e783cf22fb48fcdb
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-29 01:04:45 +0530

    keyboard design

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ import mediapipe as mp
 import numpy as np
 import time
 
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+KEYS = [
+    ['1','2','3','4','5','6','7','8','9','0'],
+    ['Q','W','E','R','T','Y','U','I','O','P'],
+    ['A','S','D','F','G','H','J','K','L'],
+    ['Z','X','C','V','B','N','M'],
+    ['SPACE','BACKSPACE','ENTER']
+]
+
+KEY_WIDTH, KEY_HEIGHT = 50, 50
+OFFSET_X, OFFSET_Y = 20, 150   # shift keyboard lower so textbox fits above
+GAP = 5
